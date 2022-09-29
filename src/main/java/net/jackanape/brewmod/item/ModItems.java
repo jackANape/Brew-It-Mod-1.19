3e5a8683212696ab65ce6262b8df27938f64c3fc
jackANape <jay-parkin91@hotmail.com>
2022-09-29 22:23:17 +1000

    Added: Chromium, Lager, Glasses, Beer Cube & Barley

--- src/main/java/net/jackanape/brewmod/item/ModItems.java
+++ src/main/java/net/jackanape/brewmod/item/ModItems.java
@@ -17,7 +17,41 @@ public class ModItems {
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BREW_TAB)));
 
+
+
+    /* ##########  Brew Mod  ########## */
+
+    /* minerals */
+    public static final RegistryObject<Item> RAW_CHROMIUM = ITEMS.register("raw_chromium",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BREW_TAB)));
+
+    public static final RegistryObject<Item> CHROMIUM_INGOT = ITEMS.register("chromium_ingot",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BREW_TAB)));
+
+
+    /* utensils */
+    public static final RegistryObject<Item> LAGER_PINT_EMPTY = ITEMS.register("lager_pint_empty",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BREW_TAB)));
+
+
+    public static final RegistryObject<Item> BEER_CUBE_EMPTY = ITEMS.register("beer_cube_empty",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BREW_TAB)));
+
+
+    /* produce */
     public static final RegistryObject<Item> LAGER_PINT = ITEMS.register("lager_pint",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BREW_TAB)));
+
+    public static final RegistryObject<Item> WORT = ITEMS.register("wort",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BREW_TAB)));
+
+    public static final RegistryObject<Item> BEER_CUBE = ITEMS.register("beer_cube",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BREW_TAB)));
+
+    public static final RegistryObject<Item> MILLED_BARLEY = ITEMS.register("milled_barley",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BREW_TAB)));
+
+    public static final RegistryObject<Item> HOPS = ITEMS.register("hops",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BREW_TAB)));
 
 
